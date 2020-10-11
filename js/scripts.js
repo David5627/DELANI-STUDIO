6cd08c838208590d9679b07d72e141d273a3f1c0
David5627 <binginaya708@gmail.com>
2020-10-11 17:23:56 +0300

    activate the mouse click hover in js

--- js/scripts.js
+++ js/scripts.js
@@ -46,3 +46,13 @@ $(document).ready(function(){
       event.preventDefault();
   });
   });
+
+  function bigImg(x) {
+    x.style.height = "64px";
+    x.style.width = "64px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "40px";
+    x.style.width = "32px";
+  }
